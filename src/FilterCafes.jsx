d96c5984cd66b86d5d3f2cca44ca2a7a79a1bb09
judetsuki <Luka9609@gmail.com>
2025-12-04 17:15:30 +0300

    1/4

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,11 @@
+const FilterCafes = (props) => {
+    return (
+        <div className='controls'>
+            <select name="subway" id="subway">
+                <option value="All">Все</option>
+            </select>
+        </div>
+    );
+};
 
+export default FilterCafes;
